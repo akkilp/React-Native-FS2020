508977691c61fec5dc056ec86ef9d76c647f5f02
Aleksi Kilponen <akkilp@utu.fi>
2020-12-24 15:11:06 +0200

    vähä siistimistä vähän

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
-=======
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
   };
->>>>>>> old_b/master
 };
